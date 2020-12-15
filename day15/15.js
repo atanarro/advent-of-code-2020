c287552ed1f043ac9cba26907cc41499245600ab
Álvaro Tanarro <alvaro.tanarro@clarity.ai>
2020-12-15 14:39:40 +0100

    day15: fastest solution

--- day15/15.js
+++ day15/15.js
@@ -3,8 +3,8 @@
 /**
  * https://adventofcode.com/2020/day/15
  * cat input.txt|node 15.js --nth 2020
- * cat input.txt|node 15.js --nth 30000000 # it takes a while:
- * 		node 15.js --nth 30000000  345,55s user 16,94s system 107% cpu 5:37,73 total
+ * cat input.txt|node 15.js --nth 30000000 # it takes a moment:
+ * 		node 15.js --nth 30000000  15,98s user 0,43s system 155% cpu 10,577 total
  */
 
 const yargs = require("yargs/yargs");
@@ -20,14 +20,17 @@ const readInterface = readline.createInterface({
   console: false,
 });
 
-const numbers = {};
+// using this map instead of a plain {} means a significant speed improvement.
+// The reason according to MDN is that
+// "performs better in scenarios involving frequent additions and removals of key-value pairs."
+const numbers = new Map();
 let currentAge = 0;
 let lastNumber = 0;
 
 function parse(line) {
   const startingNumbers = line.split(",");
   startingNumbers.forEach((n, i) => {
-    numbers[n] = { last: i + 1, beforeThanLast: i + 1 };
+    numbers.set(Number(n), { last: i + 1, beforeThanLast: i + 1 });
     lastNumber = Number(n);
   });
   currentAge = startingNumbers.length + 1;
@@ -49,21 +52,21 @@ readInterface
     while (currentAge <= nth) {
       // DEBUG console.log('Turn:', currentAge, 'lastNumber:', lastNumber);
       // DEBUG if (currentAge % 100000 === 0) console.log('Turn:', currentAge, 'numbersSize');
-      if (!numbers[lastNumber]) {
-        numbers[lastNumber] = {
+      if (!numbers.has(lastNumber)) {
+        numbers.set(lastNumber, {
           last: currentAge - 1,
           beforeThanLast: currentAge - 1,
-        };
+        });
         lastNumber = 0;
       } else {
-        numbers[lastNumber].beforeThanLast = numbers[lastNumber].last;
-        numbers[lastNumber].last = currentAge - 1;
-        const newLastNumber =
-          numbers[lastNumber].last - numbers[lastNumber].beforeThanLast;
+        numbers.set(lastNumber, {
+          beforeThanLast: numbers.get(lastNumber).last,
+          last: currentAge - 1,
+        });
         // DEBUG console.log(numbers[lastNumber], newLastNumber);
-        lastNumber = newLastNumber;
+        lastNumber =
+          numbers.get(lastNumber).last - numbers.get(lastNumber).beforeThanLast;
       }
-
       currentAge++;
     }
 
